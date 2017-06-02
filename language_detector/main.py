77b8c16507a114b31069d5d000682148e06ce4e6
Dan O <dan47573@gmail.com>
2017-06-02 00:27:16 +0000

    Brad and Dan Language Detector

--- language_detector/main.py
+++ language_detector/main.py
@@ -2,7 +2,41 @@
 
 """This is the entry point of the program."""
 
+
 def detect_language(text, languages):
-    """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+    word_count = {}
+    word_list = text.split()
+    for word in word_list:
+        if word not in word_count:
+            word_count[word] = 1
+        else:
+            word_count[word] +=1
+
+    spanish_score = 0
+    for lang in languages:
+        if lang['name'] == 'Spanish':
+            for word in lang['common_words']:
+                if word in word_count:
+                    spanish_score += word_count[word]
+
+    german_score = 0
+    for lang in languages:
+        if lang['name'] == 'German':
+            for word in lang['common_words']:
+                if word in word_count:
+                    german_score += word_count[word]
+                    
+    english_score = 0
+    for lang in languages:
+        if lang['name'] == 'English':
+            for word in lang['common_words']:
+                if word in word_count:
+                    english_score += word_count[word]
+
+    if spanish_score > german_score and spanish_score > english_score:
+        return 'Spanish'
+    elif german_score > spanish_score and german_score > english_score:
+        return 'German'
+    else:
+        return 'English'
+    
